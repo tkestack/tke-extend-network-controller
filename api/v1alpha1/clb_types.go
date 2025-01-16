2cafca3d846021f083f544915469ab55ec8a452a
roc <roc@imroc.cc>
2025-01-16 09:56:36 +0800

    Revert "update at 2025-01-15 19:15:03"
    This reverts commit 79ff74e2feefb9202d2c345759f06c95de60c881.

--- api/v1alpha1/clb_types.go
+++ api/v1alpha1/clb_types.go
@@ -28,8 +28,8 @@ type CLBSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Region of the CLB instance (e.g. ap-hongkong). For a list of all available regions, please refer to https://cloud.tencent.com/document/product/213/6091.
-	Region string `json:"region,omitempty"`
+	// Foo is an example field of CLB. Edit clb_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
 }
 
 // CLBStatus defines the observed state of CLB.
