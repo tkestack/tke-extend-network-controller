6a43ed61fef32bff7b12dcd53ae8c7a4b1d10eba
roc <roc@imroc.cc>
2025-03-25 11:54:00 +0800

    fix CanCreateLB
    Signed-off-by: roc <roc@imroc.cc>

--- internal/portpool/util/portpool.go
+++ internal/portpool/util/portpool.go
@@ -19,7 +19,7 @@ func (p *PortPool) TryNotifyCreateLB(ctx context.Context) (int, error) {
 	if err := p.Get(ctx, client.ObjectKeyFromObject(p.CLBPortPool), &pp); err != nil {
 		return 0, errors.WithStack(err)
 	}
-	if pp.CanCreateLB() {
+	if !pp.CanCreateLB() {
 		return -1, nil
 	}
 	if p.CLBPortPool.Status.State == networkingv1alpha1.CLBPortPoolStateScaling { // 已经在扩容了
