30fe8fd86af33e3d362c3abb3268495ed424d605
roc <roc@imroc.cc>
2025-03-27 20:58:07 +0800

    implement syncCLBHostPortMapping
    Signed-off-by: roc <roc@imroc.cc>

--- internal/constant/constant.go
+++ internal/constant/constant.go
@@ -1,15 +1,17 @@
 package constant
 
 const (
-	EnableCLBPortMappingsKey = "networking.cloud.tencent.com/enable-clb-port-mapping"
-	CLBPortMappingsKey       = "networking.cloud.tencent.com/clb-port-mapping"
-	CLBPortMappingStatuslKey = "networking.cloud.tencent.com/clb-port-mapping-status"
-	CLBPortMappingResultKey  = "networking.cloud.tencent.com/clb-port-mapping-result"
-	EnableCLBHostPortMapping = "networking.cloud.tencent.com/enable-clb-host-port-mapping"
-	Finalizer                = "networking.cloud.tencent.com/finalizer"
-	Ratain                   = "networking.cloud.tencent.com/retain"
-	LastUpdateTime           = "networking.cloud.tencent.com/last-update-time"
-	ProtocolTCP              = "TCP"
-	ProtocolUDP              = "UDP"
-	ProtocolTCPUDP           = "TCPUDP"
+	EnableCLBPortMappingsKey     = "networking.cloud.tencent.com/enable-clb-port-mapping"
+	CLBPortMappingsKey           = "networking.cloud.tencent.com/clb-port-mapping"
+	CLBPortMappingStatuslKey     = "networking.cloud.tencent.com/clb-port-mapping-status"
+	CLBHostPortMappingStatuslKey = "networking.cloud.tencent.com/clb-hostport-mapping-status"
+	CLBPortMappingResultKey      = "networking.cloud.tencent.com/clb-port-mapping-result"
+	CLBHostPortMappingResultKey  = "networking.cloud.tencent.com/clb-hostport-mapping-result"
+	EnableCLBHostPortMapping     = "networking.cloud.tencent.com/enable-clb-hostport-mapping"
+	Finalizer                    = "networking.cloud.tencent.com/finalizer"
+	Ratain                       = "networking.cloud.tencent.com/retain"
+	LastUpdateTime               = "networking.cloud.tencent.com/last-update-time"
+	ProtocolTCP                  = "TCP"
+	ProtocolUDP                  = "UDP"
+	ProtocolTCPUDP               = "TCPUDP"
 )
--- internal/controller/pod_controller.go
+++ internal/controller/pod_controller.go
@@ -18,12 +18,17 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	networkingv1alpha1 "github.com/imroc/tke-extend-network-controller/api/v1alpha1"
 	"github.com/imroc/tke-extend-network-controller/internal/clbbinding"
 	"github.com/imroc/tke-extend-network-controller/internal/constant"
 	"github.com/imroc/tke-extend-network-controller/pkg/eventsource"
+	"github.com/imroc/tke-extend-network-controller/pkg/kube"
+	"github.com/imroc/tke-extend-network-controller/pkg/util"
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -73,7 +78,11 @@ func (r *PodReconciler) sync(ctx context.Context, pod *corev1.Pod) (result ctrl.
 			return result, nil
 		}
 	}
-	if pod.Annotations[constant.EnableCLBHostPortMapping] != "" {
+	if pod.Annotations[constant.EnableCLBHostPortMapping] == "true" {
+		if pod.Spec.NodeName == "" {
+			log.FromContext(ctx).V(5).Info("skip host port mapping when pod not schedued to node")
+			return
+		}
 		result, err = r.syncCLBHostPortMapping(ctx, pod)
 		if err != nil {
 			return result, errors.WithStack(err)
@@ -82,7 +91,90 @@ func (r *PodReconciler) sync(ctx context.Context, pod *corev1.Pod) (result ctrl.
 	return
 }
 
+type HostPortMapping struct {
+	// 应用端口
+	ContainerPort uint16 `json:"containerPort"`
+	// 主机端口
+	HostPort uint16 `json:"hostPort"`
+	// 协议类型
+	Protocol string `json:"protocol"`
+	// 使用的端口池
+	Pool string `json:"pool"`
+	// 地域信息
+	Region string `json:"region"`
+	// 负载均衡器ID
+	LoadbalancerId string `json:"loadbalancerId"`
+	// 负载均衡器端口
+	LoadbalancerPort uint16 `json:"loadbalancerPort"`
+	// 监听器ID
+	ListenerId string `json:"listenerId"`
+}
+
+func matchHostPort(hostPort, port, lbPort, lbEndPort uint16) (uint16, bool) {
+	if lbEndPort > 0 {
+		endPort := port + (lbEndPort - lbPort)
+		if hostPort >= port && hostPort <= endPort { // 命中端口段
+			return lbPort + (hostPort - port), true
+		} else {
+			return 0, false
+		}
+	}
+	if hostPort == port { // 命中单端口监听器
+		return lbPort, true
+	}
+	return 0, false
+}
+
 func (r *PodReconciler) syncCLBHostPortMapping(ctx context.Context, pod *corev1.Pod) (result ctrl.Result, err error) {
+	cbd := &networkingv1alpha1.CLBNodeBinding{}
+	if err = r.Get(ctx, client.ObjectKey{Name: pod.Spec.NodeName}, cbd); err != nil {
+		return result, errors.WithStack(err)
+	}
+	mappings := []HostPortMapping{}
+	for _, container := range pod.Spec.Containers {
+		for _, port := range container.Ports {
+			if port.HostPort == 0 {
+				continue
+			}
+			for _, bd := range cbd.Status.PortBindings {
+				if bd.Protocol == string(port.Protocol) {
+					if lbPort, ok := matchHostPort(uint16(port.HostPort), bd.Port, bd.LoadbalancerPort, util.GetValue(bd.LoadbalancerEndPort)); ok {
+						mappings = append(mappings, HostPortMapping{
+							ContainerPort:    uint16(port.ContainerPort),
+							HostPort:         uint16(port.HostPort),
+							Protocol:         bd.Protocol,
+							Pool:             bd.Pool,
+							Region:           bd.Region,
+							LoadbalancerId:   bd.LoadbalancerId,
+							LoadbalancerPort: lbPort,
+							ListenerId:       bd.ListenerId,
+						})
+					}
+				}
+			}
+			if len(mappings) == 0 {
+				log.FromContext(ctx).V(5).Info("no hostport avaliable, make sure you enabled the clb port binding to the node")
+				return
+			}
+			val, err := json.Marshal(mappings)
+			if err != nil {
+				return result, errors.WithStack(err)
+			}
+			if pod.Annotations[constant.CLBHostPortMappingResultKey] != string(val) {
+				patchMap := map[string]any{
+					"metadata": map[string]any{
+						"annotations": map[string]string{
+							constant.CLBHostPortMappingResultKey:  string(val),
+							constant.CLBHostPortMappingStatuslKey: "Ready",
+						},
+					},
+				}
+				if err := kube.PatchMap(ctx, r.Client, pod, patchMap); err != nil {
+					return result, errors.WithStack(err)
+				}
+			}
+		}
+	}
 	return
 }
 
