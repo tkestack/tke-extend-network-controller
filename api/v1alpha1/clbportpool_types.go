87be34218358a6c6721bba7f70bea86dde03fe55
roc <roc@imroc.cc>
2025-04-25 16:20:30 +0800

    update CLBPortPool type
    Signed-off-by: roc <roc@imroc.cc>

--- api/v1alpha1/clbportpool_types.go
+++ api/v1alpha1/clbportpool_types.go
@@ -64,10 +64,12 @@ type AutoCreateConfig struct {
 // CreateLBParameters 定义创建负载均衡器的参数
 type CreateLBParameters struct {
 	// 仅适用于公网负载均衡。目前仅广州、上海、南京、济南、杭州、福州、北京、石家庄、武汉、长沙、成都、重庆地域支持静态单线 IP 线路类型，如需体验，请联系商务经理申请。申请通过后，即可选择中国移动（CMCC）、中国联通（CUCC）或中国电信（CTCC）的运营商类型，网络计费模式只能使用按带宽包计费(BANDWIDTH_PACKAGE)。 如果不指定本参数，则默认使用BGP。可通过 DescribeResources 接口查询一个地域所支持的Isp。
+	// +kubebuilder:validation:Enum=CMCC;CUCC;CTCC;BGP
 	VipIsp *string `json:"vipIsp,omitempty"`
 	// 带宽包ID，指定此参数时，网络计费方式（InternetAccessible.InternetChargeType）只支持按带宽包计费（BANDWIDTH_PACKAGE），带宽包的属性即为其结算方式。非上移用户购买的 IPv6 负载均衡实例，且运营商类型非 BGP 时 ，不支持指定具体带宽包id。
 	BandwidthPackageId *string `json:"bandwidthPackageId,omitempty"`
 	// 仅适用于公网负载均衡。IP版本，可取值：IPV4、IPV6、IPv6FullChain，不区分大小写，默认值 IPV4。说明：取值为IPV6表示为IPV6 NAT64版本；取值为IPv6FullChain，表示为IPv6版本。
+	// +kubebuilder:validation:Enum=IPV4;IPV6;IPv6FullChain
 	AddressIPVersion *string `json:"addressIPVersion,omitempty"`
 	// Target是否放通来自CLB的流量。开启放通（true）：只验证CLB上的安全组；不开启放通（false）：需同时验证CLB和后端实例上的安全组。默认值为 true。
 	LoadBalancerPassToTarget *bool `json:"loadBalancerPassToTarget,omitempty"`
@@ -75,7 +77,8 @@ type CreateLBParameters struct {
 	DynamicVip *bool `json:"dynamicVip,omitempty"`
 	// 负载均衡后端目标设备所属的网络 ID，如vpc-12345678，可以通过 DescribeVpcs 接口获取。 不填此参数则默认为当前集群所在 VPC。创建内网负载均衡实例时，此参数必填。
 	VpcId *string `json:"vpcId,omitempty"`
-	// 仅适用于公网负载均衡。目前仅广州、上海、南京、济南、杭州、福州、北京、石家庄、武汉、长沙、成都、重庆地域支持静态单线 IP 线路类型，如需体验，请联系商务经理申请。申请通过后，即可选择中国移动（CMCC）、中国联通（CUCC）或中国电信（CTCC）的运营商类型，网络计费模式只能使用按带宽包计费(BANDWIDTH_PACKAGE)。 如果不指定本参数，则默认使用BGP。可通过 DescribeResources 接口查询一个地域所支持的Isp。
+	// 指定VIP申请负载均衡。此参数选填，不填写此参数时自动分配VIP。IPv4和IPv6类型支持此参数，IPv6 NAT64类型不支持。
+	// 注意：当指定VIP创建内网实例、或公网IPv6 BGP实例时，若VIP不属于指定VPC子网的网段内时，会创建失败；若VIP已被占用，也会创建失败。
 	Vip *string `json:"vip,omitempty"`
 	// 购买负载均衡的同时，给负载均衡打上标签，最大支持20个标签键值对。
 	Tags []TagInfo `json:"tags,omitempty"`
@@ -84,6 +87,7 @@ type CreateLBParameters struct {
 	// 负载均衡实例的名称。规则：1-60 个英文、汉字、数字、连接线“-”或下划线“_”。 注意：如果名称与系统中已有负载均衡实例的名称相同，则系统将会自动生成此次创建的负载均衡实例的名称。
 	LoadBalancerName *string `json:"loadBalancerName,omitempty"`
 	// 负载均衡实例的网络类型：OPEN：公网属性， INTERNAL：内网属性。默认使用 OPEN（公网负载均衡）。
+	// +kubebuilder:validation:Enum=OPEN;INTERNAL
 	LoadBalancerType *string `json:"loadBalancerType,omitempty"`
 	// 仅适用于公网且IP版本为IPv4的负载均衡。设置跨可用区容灾时的主可用区ID，例如 100001 或 ap-guangzhou-1
 	// 注：主可用区是需要承载流量的可用区，备可用区默认不承载流量，主可用区不可用时才使用备可用区。目前仅广州、上海、南京、北京、成都、深圳金融、中国香港、首尔、法兰克福、新加坡地域的 IPv4 版本的 CLB 支持主备可用区。可通过 DescribeResources 接口查询一个地域的主可用区的列表。【如果您需要体验该功能，请通过 工单申请】
@@ -220,7 +224,7 @@ type InternetAccessible struct {
 	// - 对于公网属性和内网属性的性能容量型 CLB实例，最大出带宽的范围为1Mbps-61440Mbps。
 	// （调用CreateLoadBalancer创建LB时不指定此参数则设置为默认值10Mbps。此上限可调整）
 	InternetMaxBandwidthOut *int64 `json:"internetMaxBandwidthOut,omitempty"`
-	// 带宽包的类型，如SINGLEISP（单线）、BGP（多线）。
+	// 带宽包的类型，如 SINGLEISP（单线）、BGP（多线）。
 	// +kubebuilder:validation:Enum=SINGLEISP;BGP
 	BandwidthpkgSubType *string `json:"bandwidthpkgSubType,omitempty"`
 }
