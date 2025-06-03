cedce8b341713736351eee99f51adb62d0719cec
roc <roc@imroc.cc>
2025-06-03 20:55:53 +0800

    init rate limit
    Signed-off-by: roc <roc@imroc.cc>

--- pkg/clb/rate-limit.go
+++ pkg/clb/rate-limit.go
@@ -1,12 +1,32 @@
 package clb
 
-import "golang.org/x/time/rate"
+import (
+	"os"
+	"strconv"
+
+	"golang.org/x/time/rate"
+)
 
 var limiter map[string]*rate.Limiter = make(map[string]*rate.Limiter)
 
-func SetRateLimit(limits map[string]int) {
-	for apiName, limit := range limits {
-		limit := rate.NewLimiter(rate.Limit(limit), 1)
-		limiter[apiName] = limit
+var envMap = map[string]string{
+	"API_RATELIMIT_DESCRIBE_LOAD_BALANCERS":        "DescribeLoadBalancers",
+	"API_RATELIMIT_CREATE_LISTENER":                "CreateListener",
+	"API_RATELIMIT_DESCRIBE_LISTENERS":             "DescribeListeners",
+	"API_RATELIMIT_DELETE_LOAD_BALANCER_LISTENERS": "DeleteLoadBalancerListeners",
+	"API_RATELIMIT_BATCH_REGISTER_TARGETS":         "BatchRegisterTargets",
+	"API_RATELIMIT_DESCRIBE_TARGETS":               "DescribeTargets",
+	"API_RATELIMIT_BATCH_DEREGISTER_TARGETS":       "BatchDeregisterTargets",
+}
+
+func init() {
+	for envName, apiName := range envMap {
+		v := os.Getenv(envName)
+		if v != "" {
+			if vv, err := strconv.Atoi(v); err == nil {
+				limiter[apiName] = rate.NewLimiter(rate.Limit(vv), 1)
+				clbLog.Info("clb api rate limit", "api", apiName, "limit", vv)
+			}
+		}
 	}
 }
