6b8e3eef0ab2f4c10f88b67ae6d653ece091032f
roc <roc@imroc.cc>
2025-06-03 20:10:57 +0800

    use ApiCall: DescribeTaskStatus
    Signed-off-by: roc <roc@imroc.cc>

--- pkg/clb/wait.go
+++ pkg/clb/wait.go
@@ -12,41 +12,36 @@ import (
 )
 
 func Wait(ctx context.Context, region, reqId, taskName string) (ids []string, err error) {
-	client := GetClient(region)
 	for range 100 {
 		select {
 		case <-ctx.Done():
 			err = ctx.Err()
 			return
 		default:
-			req := clb.NewDescribeTaskStatusRequest()
-			req.TaskId = &reqId
-			before := time.Now()
-			resp, err := client.DescribeTaskStatusWithContext(ctx, req)
-			LogAPI(ctx, "DescribeTaskStatus", req, resp, time.Since(before), err)
+			res, err := ApiCall(context.Background(), "DescribeTaskStatus", region, func(ctx context.Context, client *clb.Client) (req *clb.DescribeTaskStatusRequest, res *clb.DescribeTaskStatusResponse, err error) {
+				req = clb.NewDescribeTaskStatusRequest()
+				req.TaskId = &reqId
+				res, err = client.DescribeTaskStatusWithContext(ctx, req)
+				return
+			})
 			if err != nil {
-				if IsRequestLimitExceededError(err) {
-					clbLog.Info("request limit exceeded when wait for task, retry", "reqId", reqId, "taskName", taskName)
-					time.Sleep(1 * time.Second)
-					continue
-				}
 				return nil, errors.WithStack(err)
 			}
-			switch *resp.Response.Status {
+			switch *res.Response.Status {
 			case 2: // 任务进行中，继续等待
 				time.Sleep(1 * time.Second)
 				log.FromContext(ctx).V(10).Info("task still waiting", "reqId", reqId, "taskName", taskName)
 				continue
 			case 1: // 任务失败，返回错误
 				msg := fmt.Sprintf("clb task %s failed", reqId)
-				if resp.Response.Message != nil {
-					msg += fmt.Sprintf(": %s", *resp.Response.Message)
+				if res.Response.Message != nil {
+					msg += fmt.Sprintf(": %s", *res.Response.Message)
 				}
 				return nil, errors.New(msg)
 			case 0: // 任务成功，返回nil
-				return util.ConvertPtrSlice(resp.Response.LoadBalancerIds), nil
+				return util.ConvertPtrSlice(res.Response.LoadBalancerIds), nil
 			default: // 未知状态码，返回错误
-				return nil, fmt.Errorf("unknown task status %d", *resp.Response.Status)
+				return nil, fmt.Errorf("unknown task status %d", *res.Response.Status)
 			}
 		}
 	}
